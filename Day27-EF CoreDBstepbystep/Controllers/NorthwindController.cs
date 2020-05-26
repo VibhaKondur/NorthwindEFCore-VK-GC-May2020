2edefb27c1e578da03a73d2b105e59bbc8a67109
VibhaKondur <62919352+VibhaKondur@users.noreply.github.com>
2020-05-26 17:33:46 -0400

    Northwind lab with EF Core - having issue running

---
+++ Day27-EF CoreDBstepbystep/Controllers/NorthwindController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Day27_EF_CoreDBstepbystep.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day27_EF_CoreDBstepbystep.Controllers
+{
+    
+    public class NorthwindController : Controller
+    {
+
+        private readonly NorthwindContext _context;
+
+        public NorthwindController(NorthwindController context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            //READ
+
+            List<Customers> customers = _context.Customers.ToList();
+            return View(customers);
+        }
+
+        public IActionResult CustomerForm()
+        {
+            return View();
+        }
+
+        public IActionResult EmployeeForm()
+        {
+            return View();
+        }
+
+        public IActionResult EmployeeIndex()
+        {
+            //READ
+
+            List<Employees> employees = _context.Employees.ToList();
+            return View(employees);
+
+        }
+
+        public IActionResult AddCustomer(Customers newCustomer)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Customers.Add(newCustomer);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View("CustomerForm");
+        }
+
+        public IActionResult AddEmployee(Employees newEmployee)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Employees.Add(newEmployee);
+                _context.SaveChanges();
+                return RedirectToAction("Employee Index");
+            }
+            return View("EmployeeForm");
+        }
+    }
+
+    
+}
